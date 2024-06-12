7e425f7031a4422c70925f201ffb33a1142d198d
Vladimir Trusevici <vladimir.trusevici.work.email@gmail.com>
2024-06-12 17:06:27 +0300

    TP2C-1009: feature created buyer api

---
+++ src/main/java/com/dobraccon/pain_market/customer/Customer.java
@@ -0,0 +1,12 @@
+package com.dobraccon.pain_market.customer;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Customer {
+    private Long id;
+    private String email;
+}
