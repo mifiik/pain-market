e18d1431dc163228620a1a26c055f63cf7ac13a6
Vladimir Trusevici <vladimir.trusevici.work.email@gmail.com>
2024-06-12 11:03:44 +0300

    TP2C-1009: feature project has been initialized

---
+++ src/test/java/com/dobraccon/pain_market/PainMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobraccon.pain_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
