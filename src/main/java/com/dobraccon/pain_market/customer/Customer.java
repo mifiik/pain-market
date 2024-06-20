dae9cb3943cc7224fa115abe87d9562df9206890
Vladimir Trusevici <vladimir.trusevici.work.email@gmail.com>
2024-06-20 11:37:00 +0300

    P2C-6: feature customer object has been modified as required

--- src/main/java/com/dobraccon/pain_market/customer/Customer.java
+++ src/main/java/com/dobraccon/pain_market/customer/Customer.java
@@ -1,12 +1,37 @@
 package com.dobraccon.pain_market.customer;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class Customer {
-    private Long id;
-    private String email;
+    private final Long id;
+    private final String imageUrl;
+    private final boolean lawEntity;
+    private final String email;
+    private final int phoneNumber;
+    private final String firstName;
+    private final String lastName;
+    private final String password;
+    private final String city;
+    private final String street;
+    private final int cityIndex;
+
+    public Customer(Long id, String imageUrl, boolean lawEntity, String email, int phoneNumber, String firstName,
+                    String lastName, String password, String city, String street, int cityIndex) {
+        if (phoneNumber <= 0 || cityIndex <= 0) {
+            throw new IllegalArgumentException("Invalid phone number or city index");
+        }
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.lawEntity = lawEntity;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.city = city;
+        this.street = street;
+        this.cityIndex = cityIndex;
+    }
 }
